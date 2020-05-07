417707b8a8bf2479676870e814774a4bfa3aa0fb
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-05-07 19:24:03 -0300

    Implementa página da aplicação

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, Fragment } from 'react'
+import { ThemeProvider } from 'styled-components'
+import { lightTheme, darkTheme } from './theme'
+import GlobalTheme from './global'
+import styled from 'styled-components'
 
 function App() {
+  const [theme, setTheme] = useState('light')
+
+  const toggleTheme = () => {
+    if (theme === 'light') {
+      window.localStorage.setItem('theme', 'dark')
+      setTheme('dark')
+    } else {
+      window.localStorage.setItem('theme', 'light')
+      setTheme('light')
+    }
+  }
+
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem('theme')
+    localTheme && setTheme(localTheme)
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <Fragment>
+        <GlobalTheme />
+        <Container>
+          <Title>
+            Aplicação com tema {theme === 'light' ? 'claro' : 'escuro'}!
+          </Title>
+          <ButtonChange onClick={toggleTheme}>Mudar tema</ButtonChange>
+        </Container>
+      </Fragment>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+const Container = styled.div`
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const Title = styled.h1`
+  font-size: 30px;
+`
+
+const ButtonChange = styled.button`
+  width: 100px;
+  height: 40px;
+  border-radius: 10px;
+  outline: none;
+`
+
+export default App
