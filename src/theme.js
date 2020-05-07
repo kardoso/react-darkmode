4d3ccad09e0f65680ac8d3d4bf12b4bf89f79ab7
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-05-07 19:21:46 -0300

    Configura temas

---
+++ src/theme.js
@@ -0,0 +1,10 @@
+export const lightTheme = {
+  body: '#f0f0f0',
+  text: '#333',
+  toggleBorder: '#000',
+}
+export const darkTheme = {
+  body: '#111',
+  text: '#fafafa',
+  toggleBorder: '#6B8096',
+}
